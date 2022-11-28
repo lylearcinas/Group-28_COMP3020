c25cb2993dce14d592ee8a9a3ff6859a2bf54984
Lyle Arcinas <60361950+lylearcinas@users.noreply.github.com>
2022-11-27 21:05:38 -0600

    Got JSON working with carousel

--- FillHomePage.js
+++ FillHomePage.js
@@ -1,35 +1,76 @@
 //import { books } from "./bookData"; 
 
+fillDesc(0, "featured1");
+fillDesc(1, "featured2");
+fillCarouselByTable("For You", "for-you");
+
+/*
 for(var j = 0; j < 12; j++) {
     fill(j, "list");
 }
+*/
 
-function fill(i, elementID) {
-    var value ='';    
-        value = 
-        '<div class = "article">' +
-            '<img src=\'' + books[i].img + '\'>' + 
+function fillDesc(i, elementID) {
+    var value = '<img src=\'' + books[i].img + '\'>' + 
             '<div class="main">' +
-                '<h2>' + books[i].title + '</h2>' +
-
+                '<h2><a href="#">' + books[i].title + '</a></h2>' +
                 '<div class="metadata">' +
-                    '<div class="author">' + "by" + books[i].author + '</div>' +
-
-                    '<div class="series">' + books[i].series + '</div>' +
-
-                    '<div class="year">' + books[i].year + '</div>' +
-
-                    '<div class="rating">' + books[i].rating + " stars" + '</div>' +
-
-                    '<div class="numRating">' + books[i].numRating + "M ratings" + '</div>' +
-
-                    '<div class="reviews">' + books[i].reviews + "k reviews" + '</div>' +
+                    '<div class="author">' + 
+                        'by ' + 
+                            '<a href="#" class="author">' +
+                                books[i].author + 
+                            '</a>'  +
+                        ', first published '  +
+                            '<a href="#" class="year">' + books[i].year + '</a>' + ' - ' + 
+                            '<a href="#" class="genre">' + books[i].genres[0] + '</a>' + ' - ' +
+                            '<a href="#" class="genre">' + books[i].genres[1] + '</a>' + ' - ' +
+                            '<a href="#" class="genre">' + books[i].genres[2] + '</a>' +
+                    '</div>' +
                 '</div>' +
+                '<div class = "description-container">' +
+                    '<p>' + books[i].about + '</p>' +
+                '</div>' + 
+                '<div class = "main-interact">' +
+                    '<div class="button-row">' +
+                        '<button>' +
+                            '<span class="button-icon"><i class=\'bx bxs-book-add\'></i></span> Add to Library' +
+                        '</button>' +
 
-                '<p>' + books[i].about + '</p>' +
-                '<button>' + '<span class="button-icon" style="font-size:2vw;"><i class=\'bx bxs-book-add\'></i></span> Add to Library</a>' + '</button>' 
-            '</div>' 
-        '</div>'        
+                        '<button class="star">' +
+                            books[i].rating + 
+                            '<span class="button-icon"><i class=\'bx bxs-star\'></i></span>' +
+                            '<span class="button-icon"><i class=\'bx bxs-star\'></i></span>' +
+                            '<span class="button-icon"><i class=\'bx bxs-star\'></i></span>' +
+                            '<span class="button-icon"><i class=\'bx bxs-star\'></i></span>' +
+                            '<span class="button-icon"><i class=\'bx bxs-star-half\' ></i></span>' +
+                        '</button>' +
+                    '</div>' +
+                    '<span class="bottom-text">' +
+                        '<a href="#">' +
+                            books[i].numRatings + ' ratings' +
+                        '</a>' +
+                    '</span>' +
+                    '<span class="bottom-text">' +
+                        '<a href="#">' +
+                            books[i].numReviews + ' reviews' +
+                        '</a>' +
+                    '</span>' +
+                '</div>' +
+            '</div>';
+        //'</div>';        
         document.getElementById(elementID).innerHTML += value;
 }
 
+function addPicture(i, elementID) {
+    var value = '<img src =\"' + books[i].img + '\"/>';
+    document.getElementById(elementID).innerHTML += value;
+    console.log(elementID);
+}
+
+function fillCarouselByTable(table, elementID) {
+    for (var i = 0; i < books.length; i++) {
+        if (books[i].tables.includes(table)) {
+            addPicture(i, elementID);
+        }
+    }
+}
